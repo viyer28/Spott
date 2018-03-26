da96f10917703dcd7846cc021af5efdbdd50f878
Brendan Sanderson <brendansanderson@uchicago.edu>
2018-03-26 16:58:02 -0500

    minor fix

--- Spott/Spott/C.swift
+++ Spott/Spott/C.swift
@@ -29,8 +29,8 @@ class C: NSObject {
     static var eventLightBrownColor = UIColor(red:246.0/255.0, green:194.0/255.0, blue:127.0/255.0, alpha:1.0)
     static var refid: String!
     static var userData: Dictionary<String, Any>!
-    static let databaseRoot = Auth.database().reference()
-    static let dbChat = databaseRoot.child("chats")
+//    static let databaseRoot = Auth.database().reference()
+//    static let dbChat = databaseRoot.child("chats")
     
     static func updateLocations()
     {
