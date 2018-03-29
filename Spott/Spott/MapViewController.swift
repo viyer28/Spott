dc262efed76ce093e694bb287a8ad19e26c5ccc4
viyer28 <varunviyer1@gmail.com>
2018-03-28 22:04:40 -0500

    hot map

--- Spott/Spott/MapViewController.swift
+++ Spott/Spott/MapViewController.swift
@@ -18,7 +18,7 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         MGLAccountManager.setAccessToken(MapboxAccessToken)
-        let url = URL(string: "mapbox://styles/mapbox/streets-v10")
+        let url = URL(string: "mapbox://styles/mapbox/satellite-streets-v9")
         mapView = MGLMapView(frame: CGRect(x: 0, y: 0, width: C.w, height: C.h), styleURL: url)
         mapView.delegate = self
         view.addSubview(mapView)
@@ -95,6 +95,30 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         // Always allow callouts to popup when annotations are tapped.
         return true
+    }
+    
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        
+        // Access the Mapbox Streets source and use it to create a `MGLFillExtrusionStyleLayer`. The source identifier is `composite`. Use the `sources` property on a style to verify source identifiers.
+        if let source = style.source(withIdentifier: "composite") {
+            let layer = MGLFillExtrusionStyleLayer(identifier: "buildings", source: source)
+            layer.sourceLayerIdentifier = "building"
+            
+            // Filter out buildings that should not extrude.
+            layer.predicate = NSPredicate(format: "extrude == 'true' AND height >= 0")
+            
+            // Set the fill extrusion height to the value for the building height attribute.
+            layer.fillExtrusionHeight = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "height", options: nil)
+            layer.fillExtrusionBase = MGLStyleValue(interpolationMode: .identity, sourceStops: nil, attributeName: "min_height", options: nil)
+            layer.fillExtrusionOpacity = MGLStyleValue(rawValue: 0.01)
+            
+            // Insert the fill extrusion layer below a POI label layer. If you aren’t sure what the layer is called, you can view the style in Mapbox Studio or iterate over the style’s layers property, printing out each layer’s identifier.
+            if let symbolLayer = style.layer(withIdentifier: "poi-scalerank3") {
+                style.insertLayer(layer, below: symbolLayer)
+            } else {
+                style.addLayer(layer)
+            }
+        }
     }
     
     @objc func showFriends ()
